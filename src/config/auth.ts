aee28e0a6f1817e6d114286b8a43bf7f39fd72fc
Gabriela Papin <gabrielapapin20@gmail.com>
2023-04-19 14:23:37 -0300

    feat: auth
    config e service de autenticação jwt criados.

---
+++ src/config/auth.ts
@@ -0,0 +1,15 @@
+import { sign } from 'jsonwebtoken';
+
+const jwtConfig = {
+  expiresIn: '1d',
+  secret: process.env.JWT_SECRET || '',
+};
+
+export default function tokenSignature(userId: string): string {
+  const token = sign({ data: userId }, jwtConfig.secret, {
+    expiresIn: jwtConfig.expiresIn,
+    algorithm: 'HS256',
+  });
+
+  return token;
+}
