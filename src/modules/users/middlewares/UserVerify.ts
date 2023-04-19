3f0d3ab643023be3fabaca18274d535801c85b9b
Gabriela Papin <gabrielapapin20@gmail.com>
2023-04-19 19:09:45 -0300

    feat: verificação
    criação da verificação dos inputs

---
+++ src/modules/users/middlewares/UserVerify.ts
@@ -0,0 +1,26 @@
+import AppError from '@shared/erros/AppError';
+import * as Joi from 'joi';
+
+interface ICreateVerify {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export function createVerify({
+  username,
+  email,
+  password,
+}: ICreateVerify): void {
+  const schema = Joi.object({
+    username: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  });
+
+  const { value, error } = schema.validate({ username, email, password });
+
+  if (error) {
+    throw new AppError(error.message, 400);
+  }
+}
