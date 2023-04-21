eef982babeb68f8386595c07bdc33711c7d0e135
Gabriela Papin <gabrielapapin20@gmail.com>
2023-04-21 17:53:25 -0300

    fix: ForgotPassword
    correção da requisição do token

--- src/modules/users/controllers/ForgotPasswordController.ts
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 import SendForgotPasswordService from '@modules/users/services/SendForgotPasswordService';
+import { emailVerify } from '@modules/users/middlewares/UserVerify';
 
 export default class ForgotPasswordController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { email } = req.body;
+
+    emailVerify({ email });
 
     const sendForgotPassword = new SendForgotPasswordService();
 
